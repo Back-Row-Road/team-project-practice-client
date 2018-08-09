552b5f5284e17211f3d66093f0b57ed165facaee
eabrigham <eabrigham@gmail.com>
2018-08-09 14:56:17 -0400

    Added create post button and file structure
    Added click handler for post button

---
+++ assets/scripts/post/api.js
@@ -0,0 +1,9 @@
+
+
+const createStore = function (data) {
+    console.log('In api.js and button was clicked')
+}
+
+module.exports = {
+    createStore
+}
---
+++ assets/scripts/post/events.js
@@ -0,0 +1,16 @@
+const getFormFields = require('../../../lib/get-form-fields.js')
+
+const onCreatePost = function (event) {
+    event.preventDefault()
+    const data = getFormFields(event.target)
+    console.log('In onCreatePost in post events')
+    // do api
+}
+
+const addHandler = () => {
+    $('#create-post-button').on('click', onCreatePost)
+}
+
+module.exports = {
+    addHandler
+}
