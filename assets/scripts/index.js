6c46557fca9ec8bb008782997f521016b12e5c35
eabrigham <eabrigham@gmail.com>
2018-08-09 14:56:17 -0400

    Added create post button and file structure
    Added click handler for post button

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,9 +2,9 @@
 const blogApi = require("./blog/blogs-Api.js");
 const blogs = require("./blog/blogs-stack.js");
 
+
 const authEvents = require('./auth/events.js')
 const postEvents = require('./post/events.js')
-
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
