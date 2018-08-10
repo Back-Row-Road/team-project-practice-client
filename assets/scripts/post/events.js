785fe34eaa4996d56719fb5ecf418b1310750992
eabrigham <eabrigham@gmail.com>
2018-08-10 13:19:01 -0400

    WIP Made create post modal, working on post ajax
    Put the create post form in modal. Currently getting 500 server error,
    need to identify correct form of data.

--- assets/scripts/post/events.js
+++ assets/scripts/post/events.js
@@ -1,14 +1,18 @@
 const getFormFields = require('../../../lib/get-form-fields.js')
+const api = require('./api.js')
 
 const onCreatePost = function (event) {
-  event.preventDefault()
-  const data = getFormFields(event.target)
-  console.log('In onCreatePost in post events')
-  // do api
+    event.preventDefault()
+    const data = getFormFields(event.target)
+    console.log('In onCreatePost in post events and the data is ', data)
+    api.createPost(data)
 }
 
 const addHandler = () => {
-  // $('#create-post-button').on('click', onCreatePost)
+    $('#create-post-button').on('click', () =>
+    $("#create-post-modal").css("display", "block"))
+
+    $("#create-post").on("submit", onCreatePost)
 }
 
 module.exports = {
