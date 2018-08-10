4d68e8bf7d66ebdc28f0ad95904b2ef6cf4b0468
lahdesign <lahdesign@gmail.com>
2018-08-10 11:27:44 -0400

    working on page crud operations

---
+++ assets/scripts/page/api.js
@@ -0,0 +1,50 @@
+const config = require('./../config.js')
+const store = require('./../store.js')
+
+const getPages = function () {
+  return $.ajax({
+    method: 'GET',
+    url: config.apiUrl + '/pages',
+  })
+}
+
+const createPage = function (data) {
+  return $.ajax({
+    method: 'POST',
+    url: config.apiUrl + '/pages',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data
+  })
+}
+
+const updatePage = function (data) {
+  return $.ajax({
+    method: 'PATCH',
+    url: config.apiUrl + '/pages/' + data.page.id, // TODO figure out what data & get id
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data
+  })
+}
+
+const deletePage = function (data) {
+  return $.ajax({
+    method: 'DELETE',
+    url: config.apiUrl + '/pages/' + data.page.id, // TODO figure out what data & get id
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+
+
+module.exports = {
+  getPages,
+  createPage,
+  updatePage,
+  deletePage
+}
