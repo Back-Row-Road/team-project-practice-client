9556e0b948467900a2c0b5541b78edac634cea69
rachael miller design <rachael@rachaelmillerdesign.com>
2018-08-11 06:39:37 -0400

    this commit includes incomplete code for 'post' ajax crud in api, events, and index.html

--- assets/scripts/post/events.js
+++ assets/scripts/post/events.js
@@ -2,19 +2,28 @@ const getFormFields = require('../../../lib/get-form-fields.js')
 const api = require('./api.js')
 
 const onCreatePost = function (event) {
-    event.preventDefault()
-    const data = getFormFields(event.target)
-    console.log('In onCreatePost in post events and the data is ', data)
-    api.createPost(data)
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  console.log('In onCreatePost in post events and the data is ', data)
+  api.createPost(data)
 }
 
+const onUpdatePost = function (event) {
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  console.log('In onUpdatePost in post events and the data is ', data)
+  api.updatePost(data)
+}
 const addHandler = () => {
-    $('#create-post-button').on('click', () =>
-    $("#create-post-modal").css("display", "block"))
+  $('#create-post-button').on('click', () =>
+    $('#create-post-modal').css('display', 'block'))
+  $('#create-post').on('submit', onCreatePost)
 
-    $("#create-post").on("submit", onCreatePost)
+  $('#update-post-button').on('click', () =>
+    $('#update-post-modal').css('display', 'block'))
+  $('#update-post').on('submit', onUpdatePost)
 }
 
 module.exports = {
-    addHandler
+  addHandler
 }
