773b16dab12e41cb58861b3be9458d09b8b330bf
rachael miller design <rachael@rachaelmillerdesign.com>
2018-08-11 06:39:37 -0400

    this commit includes incomplete code for 'post' ajax crud in api, events, and index.html

--- assets/scripts/post/api.js
+++ assets/scripts/post/api.js
@@ -2,41 +2,41 @@ const config = require('./../config.js')
 const store = require('./../store.js')
 
 const getPosts = function () {
-    return $.ajax({
-        method: 'GET',
-        url: config.apiUrl + '/posts',
-    })
+  return $.ajax({
+    method: 'GET',
+    url: config.apiUrl + '/posts',
+  })
 }
 
 const createPost = function (data) {
-    return $.ajax({
-        method: 'POST',
-        url: config.apiUrl + '/posts',
-        headers: {
-            Authorization: 'Token token=' + store.user.token
-        },
-        data
-    })
+  return $.ajax({
+    method: 'POST',
+    url: config.apiUrl + '/posts',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data
+  })
 }
 
 const updatePost = function (data) {
-    return $.ajax({
-        method: 'PATCH', 
-        url: config.apiUrl + '/posts/' + data.post.id, // TODO figure out what data & get id
-        headers: {
-            Authorization: 'Token token=' + store.user.token
-        },
-        data
-    })
+  return $.ajax({
+    method: 'PATCH',
+    url: config.apiUrl + '/posts/' + data.post.id, // TODO figure out what data & get id
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data
+  })
 }
 
 const deletePost = function (data) {
-    return $.ajax({
-        method: 'DELETE',
-        url: config.apiUrl + '/posts/' + data.post.id, // TODO figure out what data & get id
-        headers: {
-            Authorization: 'Token token=' + store.user.token
-        }
+  return $.ajax({
+    method: 'DELETE',
+      url: config.apiUrl + '/posts/' + data.post.id, // TODO figure out what data & get id
+      headers: {
+        Authorization: 'Token token=' + store.user.token
+      }
     })
 }
 
