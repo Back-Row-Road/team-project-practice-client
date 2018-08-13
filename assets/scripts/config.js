ac26958577dcaf28e629d06acf5a8c10e43f7d77
eabrigham <eabrigham@gmail.com>
2018-08-13 09:03:25 -0400

    Removed extra slash from production url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://thawing-dawn-36769.herokuapp.com/',
+  production: 'https://thawing-dawn-36769.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
